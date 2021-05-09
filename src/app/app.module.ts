64a68316aeb26967cf5629275c51c92e99151e81
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2021-05-09 11:20:56 +0100

    Basic structure of the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home/home.component';
+import { CategoryComponent } from './home/category/category.component';
+import { ArticlesComponent } from './home/articles/articles.component';
+import { FeedbackComponent } from './home/feedback/feedback.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, CategoryComponent, ArticlesComponent, FeedbackComponent, NavbarComponent, FooterComponent, HeaderComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
